34658193efe2f6176f811c3943afbb350ccc954c
Gad Jacobs <gadexofficial@gmail.com>
2023-02-17 07:19:06 +0100

    Do not load more if no more items to display

--- hooks/use-api-data.ts
+++ hooks/use-api-data.ts
@@ -1,16 +1,21 @@
-import { useEffect, useState } from "react"
-import instance from "../utils/axios"
+import { useEffect, useState } from 'react';
+import instance from '../utils/axios';
 
 export const useApiData = <T>(path: string, defaultValue: any): T => {
-  const [ data, setData ] = useState<T>(defaultValue)
+  const [data, setData] = useState<T>(defaultValue);
 
   useEffect(() => {
-    instance.get<T>(path).catch(err => err.response).then(response => {
-      setData(response.data)
-    })
-  }, [path])
+    instance
+      .get<T>(path)
+      .catch((err) => err.response)
+      .then((response) => {
+        if (response.data.length) {
+          setData(response.data);
+        }
+      });
+  }, [path]);
 
-  return data
-}
+  return data;
+};
 
-export default useApiData
+export default useApiData;
