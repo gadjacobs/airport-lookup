46c0d26c0277fd625999434a7ab0d512d30bd226
Gad Jacobs <gadexofficial@gmail.com>
2023-02-17 03:36:17 +0100

    Tweak api to add page number and page limit

--- pages/api/airports.ts
+++ pages/api/airports.ts
@@ -3,7 +3,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { allAirports } from '../../models/airport'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const airports = await allAirports()
+  const airports = await allAirports();
 
-  res.status(200).json(airports)
+  const { page = 1, limit = 20 } = req.query;
+
+  const startIndex = (Number(page) - 1) * Number(limit);
+  const endIndex = Number(page) * Number(limit);
+
+  res.status(200).json(airports.slice(startIndex, endIndex));
 }
